f1dd239148027bba679bbd5c4e706df5d6836b81
MdTanzil <mdtanzil.py@gmail.com>
2022-05-22 04:35:54 +0600

    first Django model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    text = models.CharField(max_length=250,blank=False)
+    
+    def __str__(self):
+        return self.text
+    
+    
